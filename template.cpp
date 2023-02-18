3a4edf13b2b49bc199d4ebf6fb0e6b710c131c20
slender789 <esau.garcia.007@gmail.com>
2023-02-18 15:15:13 -0700

    Comienzo

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+//assert(x>0) si falla da RTE
+using namespace std;
+#define endl '\n'
+#define DBG(x) cerr<<#x<< "=" << (x) << endl;
+#define RAYA cerr<<"======================"<<endl;
+#define RAYAS cerr<<"......................"<<endl;
+//#define DBG(x) ;
+//#define RAYA ;
+//#define RAYAS ;
+
+//--------------------SOLBEGIN---------------------------------
+int main() {
+	ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+	int tC;
+
+	cin >> tC;
+	while (tC--) {
+
+	}
+
+}
+//--------------------EOSOLUTION---------------------------------
